d2ccbcd7c15f59924d6623d64953cc563cdbad10
Spencer Rosenberger <sr793514@ohio.edu>
2018-10-10 19:09:33 -0400

    Enemy Chase
    That sweet sweet AI development

---
+++ FamilyIsEverything-Master-Project/Assets/Scripts/Enemy/EnemyChase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChase : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent myAgent;
+    public Transform target;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myAgent.SetDestination(target.position);	
+	}
+}
