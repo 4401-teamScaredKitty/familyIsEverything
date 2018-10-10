a948032f6eaa6ee7d6b1e56dfe8d4eb0c2e59dae
Mitchell A. Cook <mc564615@ohio.edu>
2018-10-10 17:06:32 -0400

    Inventory Implemtation
    The programming is now implemented into the project under Scripts->Inventory folder. The empty object in "Santa's Workshop" shows the primal version of the Inventory.

---
+++ FamilyIsEverything-Master-Project/Assets/Scripts/Inventory_Scripts/Inventory.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static Inventory instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory found");
+            return;
+        }
+
+        instance = this;
+    }
+
+    #endregion
+
+    public delegate void OnItemChange();
+    public OnItemChange onItemChangeCallback;
+
+    public int space = 20;
+
+
+    public List<Item> items = new List<Item>();
+
+    public bool Add(Item item)
+    {
+        if (!item.isDefaultitem)
+        {
+            if (items.Count >= space)
+            {
+                Debug.Log("Not enough Room, Jackass");
+                return false;
+            }
+
+            items.Add(item);
+
+            if (onItemChangeCallback != null)
+                onItemChangeCallback.Invoke();
+        }
+
+        return true;
+
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+
+        if (onItemChangeCallback != null)
+            onItemChangeCallback.Invoke();
+    }
+}
