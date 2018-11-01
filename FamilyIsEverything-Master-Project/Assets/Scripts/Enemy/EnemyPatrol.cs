966ded6f17c5469e29ac94524cb432528f546571
Spencer Rosenberger <sr793514@ohio.edu>
2018-10-31 21:47:15 -0400

    Going Ham on AI
    AI now has patrol points and can be set up to goto exact patrol points or even have it choose which one on its own

---
+++ FamilyIsEverything-Master-Project/Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    //Dictates whether the agent waits on each node.
+    [SerializeField]
+    bool _patrolWaiting;
+
+    //Total time we wait at each pint
+    [SerializeField]
+    float _totalWaitTime = 3f;
+
+    //The chance of switching direction
+    [SerializeField]
+    float _switchProbability = 0.2f;
+
+    //The list of all patrol nodes to visit
+    [SerializeField]
+    List<Waypoints> _patrolPoints;
+
+    //Private variables for base behaviour
+    NavMeshAgent _navMeshAgent;
+    int _currentpatrolIndex;
+    bool _travelling;
+    bool _waiting;
+    bool _patrolForward;
+    float _waitTimer;
+
+    public void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent component is not attached to" + gameObject.name);
+        }
+        else
+        {
+            if (_patrolPoints != null && _patrolPoints.Count >= 2)
+            {
+                _currentpatrolIndex = 0;
+                SetDestination();
+            }
+            else
+            {
+                Debug.Log("Insufficient patrol points for basic patrolling behaviour");
+            }
+        }
+    }
+
+    public void Update()
+    {
+        //Check if we are close to destination
+        if (_travelling && _navMeshAgent.remainingDistance <= 1.0f)
+        {
+            _travelling = false;
+
+            //If we are going to wait then wait
+            if (_patrolWaiting)
+            {
+                _waiting = true;
+                _waitTimer = 0f;
+            }
+            else
+            {
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+
+        //Instead if we are waiting
+        if (_waiting)
+        {
+            _waitTimer += Time.deltaTime;
+            if (_waitTimer >= _totalWaitTime)
+            {
+                _waiting = false;
+
+                ChangePatrolPoint();
+                SetDestination();
+
+            }
+
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (_patrolPoints != null)
+        {
+            Vector3 targetVector = _patrolPoints[_currentpatrolIndex].transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+            _travelling = true;
+        }
+    }
+
+    private void ChangePatrolPoint()
+    {
+        if (UnityEngine.Random.Range(0f, 1f) <= _switchProbability)
+        {
+            _patrolForward = !_patrolForward;
+        }
+        if (_patrolForward)
+        {
+            _currentpatrolIndex = (_currentpatrolIndex + 1) % _patrolPoints.Count;
+        }
+        else
+        {
+            if (--_currentpatrolIndex < 0)
+            {
+                _currentpatrolIndex = _patrolPoints.Count - 1;
+            }
+        }
+    }
+}
---
+++ FamilyIsEverything-Master-Project/Assets/Scripts/Enemy/Waypoints.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    [SerializeField]
+    protected float debugDrawRadius = 1.0f;
+
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+	}
+}
