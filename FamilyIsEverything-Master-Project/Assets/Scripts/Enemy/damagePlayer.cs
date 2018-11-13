373cbe98b6a840b86b23e2f2e2653cec6a82172c
Spencer Rosenberger <sr793514@ohio.edu>
2018-11-13 12:09:11 -0500

    Enemy can "Kill Player" loads game over scene

---
+++ FamilyIsEverything-Master-Project/Assets/Scripts/Enemy/damagePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class damagePlayer : MonoBehaviour
+{
+
+    public int playerHealth = 30;
+    int damage = 10;
+
+
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene("Game_Over", LoadSceneMode.Additive);
+        }
+    }
+
+   
+
+}
